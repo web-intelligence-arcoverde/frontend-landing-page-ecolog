25c2758e7dc10ce37e5cca3f8684429d61eaff4d
Paulo Marques <paulinho_marques2012@hotmail.com>
2022-01-18 10:00:21 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import react from "react";
+import * as S from './styled'
+
+
+ const Home = () => {
+
+    return (
+        <S.Container>
+            <h1>Pagina principal</h1>
+        </S.Container>
+    )
+}
+
+export default Home;
